2593a145dc6f9e1280cb95a1fa0e3ecb4837463a
codwerk <louis_widdy@live.fr>
2020-09-23 23:11:33 +0200

    card html

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template lang="pug">
   #app
-    app-shoes(:shoes="shoes")
+    h1.titre-shop SHOP
+    .main.bg-grid
+      app-shoes(:shoes="shoes")
 </template>
 
 <script>
 
-import Shoe from './components/Shoe'
+import Shoe from './components/Shoes'
 
 export default {
   name: 'App',
@@ -47,8 +49,55 @@ export default {
 
 <style lang="scss">
 
+@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap');
 
-@import url('./assets/scss/index.scss');
+/*-- VARIABLES CSS--*/
+/*Colores*/
+
+  $first-color: #E3F8FF;
+  $second-color: #DCFAFB;
+  $third-color: #FFE8DF;
+  $accent-color: #FF5151;
+  $dark-color: #161616;
+
+
+/*Tipografia responsive*/
+  $body-font: 'Open Sans';
+  $h1-font-size: 1.5rem;
+  $h3-font-size: 1rem;
+  $normal-font-size: 0.938rem;
+  $smaller-font-size: 0.75rem;
+
+@media screen and (min-width: 768px){
+  
+    $h1-font-size: 2rem;
+    $normal-font-size: 1rem;
+    $smaller-font-size: 0.813rem;
+    
+}
+
+/*-- BASE --*/
+*,::after,::before{
+    box-sizing: border-box;
+}
+body{
+    margin: 2rem 0 0 0;
+    background-color: #fff;
+    color: $dark-color;
+    font-family: $body-font;
+}
+h1{
+    font-size: $h1-font-size;
+}
+img{
+    max-width: 100%;
+    height: auto;
+}
+a{
+    text-decoration: none;
+}
+
+ul{list-style:none;padding: 0;}
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -56,5 +105,19 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+
+  //layout
+  .main {
+    padding: 2rem 0;
+  }
+
+  .bg-grid {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minimax(220px, 1fr));
+    max-width: 1200px;
+    margin-left: 2.5rem;
+    margin-right: 2.5rem;
+    align-items: center;
+  }
 }
 </style>
---
+++ src/components/Shoes.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+    .wrapper__article
+      Article(v-for="shoe in shoes").card
+        .card__img
+          //image
+          img(:src="shoe.img", :alt="shoe.name")
+        .card__name
+          p {{ shoe.name }}
+        .card__details
+          a(href="").card__icon
+            IosHeartEmptyIcon
+          ul
+            li.card__price.card__price--before ${{ shoe.priceBefore }}
+            li.card__price.card__price--now ${{ shoe.priceNow }}
+          a(href="").card__icon 
+            IosCartIcon
+
+
+</template>
+
+<script>
+
+import IosHeartEmptyIcon from 'vue-ionicons/dist/ios-heart-empty.vue'
+import IosCartIcon from 'vue-ionicons/dist/ios-cart.vue'
+
+ export default {
+  props: ['shoes'],
+  components: {
+    IosHeartEmptyIcon,
+    IosCartIcon
+  },
+   data: () => {
+     return {}
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .wrapper__article {
+    
+    .card {
+
+    }
+  }
+</style>
