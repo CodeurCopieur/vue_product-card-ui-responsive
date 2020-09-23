8f2d9a8f8a324bb92f7954e9de33fe631850c899
codwerk <louis_widdy@live.fr>
2020-09-23 21:43:39 +0200

    creer le composant Shoe, l'objet Shoes

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="pug">
+  #app
+    app-shoes(:shoes="shoes")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Shoe from './components/Shoe'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    'app-shoes': Shoe
+  },
+  data () {
+    return {
+      shoes: [
+        {
+          img: require('./assets/img/img1.png'),
+          name: 'AIR ZOOM PEGASUS',
+          priceBefore: 990.00,
+          priceNow: 749.00
+        },
+        {
+          img: require('./assets/img/img2.png'),
+          name: 'AIR ZOOM PEGASUS',
+          priceBefore: 990.00,
+          priceNow: 749.00
+        },
+        {
+          img: require('./assets/img/img3.png'),
+          name: 'AIR ZOOM PEGASUS',
+          priceBefore: 990.00,
+          priceNow: 749.00
+        },
+        {
+          img: require('./assets/img/img4.png'),
+          name: 'AIR ZOOM PEGASUS',
+          priceBefore: 990.00,
+          priceNow: 749.00
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+
+@import url('./assets/scss/index.scss');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Shoe.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    .container
+      li(v-for="shoe in shoes") {{ shoe.name }}
+</template>
+
+<script>
+ export default {
+  props: ['shoes'],
+   data: () => {
+     return {}
+   }
+  }
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
